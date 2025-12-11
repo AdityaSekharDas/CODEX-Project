ed604071c7d282f9efeebd9769f7279826f5bae9
jyotiswadhindibya-bit <jyotiswadhindibya@gmail.com>
2025-12-11 23:14:47 +0530

    Done stop button work and bubble pop with random number

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 const startBtn = document.getElementById("startBtn");
 const rightBox = document.getElementById("gameArea");
+const stopBtn = document.getElementById("stopBtn");
+const stackBox = document.getElementById("stackBox");
+const sortedBox = document.getElementById("sortedBox");
+
+let stack=[];
+let intervalId=null;
 
 startBtn.addEventListener("click", function () {
+if(intervalId) 
+ return;
 
-  setInterval(function () {
-    let bubble = document.createElement("div");
+stack=[];
+stackBox.innerHTML="";
+sortedBox.innerHTML = "";
+rightBox.innerHTML = ""; 
+
+intervalId=setInterval(function () {
+   let bubble = document.createElement("div");
     bubble.className = "bubble";
 
     const maxLeft = rightBox.clientWidth - 60;
     bubble.style.left = Math.floor(Math.random() * maxLeft) + "px";
 
-    rightBox.appendChild(bubble);
+    bubble.addEventListener("mousedown", function() {
+        let num = Math.floor(Math.random() * 100) + 1;
+        stack.push(num);
+        let stackItem = document.createElement("div");
+        stackItem.innerText = num;
+        stackBox.appendChild(stackItem);
+        bubble.remove();
+    });
 
-    setTimeout(function () {
-      bubble.remove();
+     rightBox.appendChild(bubble);
+       
+     setTimeout(function () {
+       bubble.remove();
     }, 3000);
 
   }, 600);
 
 });
+
+stopBtn.addEventListener("click",function (){
+  if(!intervalId) 
+    return;
+
+  clearInterval(intervalId);
+  intervalId=null;
+  rightBox.innerHTML = "";
+  performBubbleSort();
+  displaySortedOutput();
+});
+
+function performBubbleSort(){
+
+}
+
+function displaySortedOutput(){
+
+}
