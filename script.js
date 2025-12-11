ffa1d97e32b54f38c8b6d18fc0a365cc9a794b0f
AdityaSekharDas <adityasekhardas09@gmail.com>
2025-12-11 21:27:36 +0530

    updated

---
+++ script.js
@@ -0,0 +1,24 @@
+const startBtn = document.getElementById("startBtn");
+const rightBox = document.getElementById("gameArea");
+
+startBtn.addEventListener("click", function () {
+
+  setInterval(function () {
+    let bubble = document.createElement("div");
+    bubble.className = "bubble";
+
+    
+    const maxLeft = rightBox.clientWidth - 60;
+    bubble.style.left = Math.floor(Math.random() * maxLeft) + "px";
+
+    
+    rightBox.appendChild(bubble);
+
+    
+    setTimeout(function () {
+      bubble.remove();
+    }, 3000);
+
+  }, 600);
+
+});
